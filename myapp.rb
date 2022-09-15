74f4b5e3e71311727c2ce7ef5f0d991d533494d3
linlin <1208215066@qq.com>
2022-09-15 14:38:36 +0800

    增加了一些测试文件，用来实现页面的layout，页面的css使用，参数的使用

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,55 @@
 require 'sinatra'
 
+
+
+class Stream
+  def each
+    100.times { |i| yield "#{i}\n" }
+  end
+end
+
+get('/each') { Stream.new }
+
+
 get '/' do
   'hello world'
 end
+
+get '/blog' do
+  erb :index
+end
+
+get '/post' do
+  erb :content, :layout => :post
+end
+get '/app' do
+  erb :index, :layout => :application
+end
+
+get '/apple' do
+  code = "<%= Time.now %>"
+  erb code
+end
+
+
+get '/hello/:name' do
+  "Hello #{params['name']}!"
+end
+
+get '/hi/:color' do |n|
+  "Hi#{params['color']}!"
+end
+
+get '/say/*/to/*' do
+  # matches /say/hello/to/world
+  params['splat'] # => ["hello", "world"]
+end
+
+get '/download/*.*' do
+  # matches /download/path/to/file.xml
+  params['splat'] # => ["path/to/file", "xml"]
+end
+
+
+
 
