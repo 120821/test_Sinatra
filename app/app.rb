1198d7f6a4a004bd6caacd9a6379eb273ac62f73
linlin <1208215066@qq.com>
2022-09-16 10:38:22 +0800

    修改了index页面，输出hello，/user/?页面，user数据为空

--- app/app.rb
+++ app/app.rb
@@ -9,18 +9,18 @@ class App < Sinatra::Base
     content_type :json
   end
 
-  get '/' do
-    p 'Hello!'
-  end
-
-  get '/users/?' do
-    @users = User.all
-    @users.to_json
-  end
-
-  get '/users/:id/?' do
-    @user = User.find_by_id(params[:id])
-    @user.to_json
-  end
 end
 
+get '/' do
+  'Hello!'
+end
+
+get '/users/?' do
+  @users = User.all
+  @users.to_json
+end
+
+get '/users/:id/?' do
+  @user = User.find_by_id(params[:id])
+  @user.to_json
+end
